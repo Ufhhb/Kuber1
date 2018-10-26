16d28837a834e55bb4169e27b96ee12d7e84bb57
created with ansible <user10@mc-bastion.c.dmk2-oct-2018.internal>
2018-10-26 11:08:54 +0000

    first commit

---
+++ gcloud.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+gcloud beta container --project "dmk2-oct-2018" clusters create "userXX" --zone "europe-west1-c" --username "admin" --cluster-version "1.9.7-gke.6" --machine-type "n1-standard-2" --image-type "COS" --disk-type "pd-standard" --disk-size "100" --scopes "https://www.googleapis.com/auth/compute","https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" --num-nodes "2" --no-enable-cloud-logging --no-enable-cloud-monitoring --network "projects/dmk2-oct-2018/global/networks/default" --subnetwork "projects/dmk2-oct-2018/regions/europe-west1/subnetworks/default" --enable-legacy-authorization --addons HorizontalPodAutoscaling,HttpLoadBalancing,KubernetesDashboard --enable-autoupgrade --enable-autorepair
+
+
